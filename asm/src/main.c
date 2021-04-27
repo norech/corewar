a6d52ad4103ec8e6f8ab596d284a136b1f0a1673
Alexis Cheron <alexisnorech@gmail.com>
2021-04-27 23:00:28 +0400

    added main files

---
+++ asm/src/main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** main
+** File description:
+** Source code
+*/
+
+#include <my.h>
+#include <my/io.h>
+
+int main(int ac UNUSED, char *av[] UNUSED)
+{
+    my_printf("Hello world!");
+}
