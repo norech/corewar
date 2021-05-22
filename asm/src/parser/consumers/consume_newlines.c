bcaac9328810f00bdf5b224973a296856d68baeb
ludovicplt <ludovic.peltier@epitech.eu>
2021-05-22 21:20:17 +0400

    adding skipping commentary

--- asm/src/parser/consumers/consume_newlines.c
+++ asm/src/parser/consumers/consume_newlines.c
@@ -7,12 +7,23 @@
 
 #include "asm/parser.h"
 
+void consume_commentary(parser_t *parser)
+{
+    if (*parser->pos == '#')
+        do {
+            parser->pos++;
+        } while (*parser->pos != '\n' && *parser->pos != '\r'
+            && *parser->pos != '\0');
+}
+
 int consume_newlines(parser_t *parser)
 {
     int i = 0;
 
+    consume_commentary(parser);
     while (*parser->pos == '\n' || *parser->pos == '\r') {
         parser->pos++;
+        consume_commentary(parser);
         if (*parser->pos != '\r')
             i++;
     }
